d1969ab2924dde5357eb8f54188d8bfd169ff599
Zyinat11 <zyinat.kurbanbaeva.kk.gmail.com>
2022-04-13 15:20:54 +0300

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FileWriter writer=new FileWriter("Alfavit");
+        writer.write("Aa\n" +
+                "Bb\n" +
+                "Cc\n" +
+                "Dd\n" +
+                "Ee\n" +
+                "Ff\n" +
+                "Gg\n" +
+                "Hh\n" +
+                "Ii\n" +
+                "Gg\n" +
+                "Kk\n" +
+                "Ll\n" +
+                "Mm\n" +
+                "Nn\n" +
+                "Oo\n" +
+                "Pp\n" +
+                "Qq\n" +
+                "Rr\n" +
+                "Ss\n" +
+                "Tt\n" +
+                "Vv\n" +
+                "Ww\n" +
+                "Xx\n" +
+                "Yy\n" +
+                "Zz\n" +
+                "0\n" +
+                "1\n" +
+                "2\n" +
+                "3\n" +
+                "4\n" +
+                "5\n" +
+                "6\n" +
+                "7\n" +
+                "8\n" +
+                "9\n");
+        writer.close();
+        FileReader reader=new FileReader("Alfavit");
+        Scanner scanner=new Scanner(reader);
+        while (scanner.hasNext()) {
+
+
+            System.out.println(scanner.nextLine());
+
+        }
+
+    }
+}
